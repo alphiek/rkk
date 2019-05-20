e3639209ff3ee5c1e5ef42420af93392833c5395
alphiek <43937504+alphiek@users.noreply.github.com>
2019-05-20 21:10:34 +0300

    Breakpoint Revision
    React Socks Breakpoint Revision

--- src/pages/WebDevelopment.js
+++ src/pages/WebDevelopment.js
@@ -7,7 +7,8 @@ import { BreakpointProvider} from 'react-socks'
 import { setDefaultBreakpoints } from 'react-socks'
 
 setDefaultBreakpoints([
-  { tablet: 0 },
+  { mobile: 0 },
+  { tablet: 824 },
   { desktop: 1025 }
 ]);
 
