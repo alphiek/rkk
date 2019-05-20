514b9208168863256b0017ff9277ce714fad70d3
Nick Feltham <43998553+nick-feltham@users.noreply.github.com>
2019-05-20 19:06:45 +0300

    Update Video.js

--- src/components/Development/Video.js
+++ src/components/Development/Video.js
@@ -1,20 +1,17 @@
-import React from 'react'
 import gif from '../../images/GIF/rkkMock.gif'
 import styled from 'styled-components'
 import { RightWrapper } from '../Global/ServiceFlexWrapper'
+import Fade from 'react-reveal/Fade'
 
 const GIF = styled.img`
   width: 20em;
-  margin-top: -2em;
-  @media only screen and (max-width: 1024px) {
-    margin-top: 0;
-    margin-bottom: -2em;
-  }
-`
+@@ -15,9 +14,7 @@ const GIF = styled.img`
 const Video = () => {
   return (
     <RightWrapper>
+    <Fade right>
      <GIF src={gif} alt='Laptop with moving code and site preview' />
+     </Fade>
     </RightWrapper>
   )
 }
